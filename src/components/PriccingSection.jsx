3304c950b0994c0cca70f3bab7d805be9ab48d30
ibnuziyad <ibnuziyad66@gmail.com>
2026-05-07 23:30:38 +0700

    sec commit

--- src/components/PriccingSection.jsx
+++ src/components/PriccingSection.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Check, X } from "lucide-react";
 
-const WA_NUMBER = "628951183961";
+const WA_NUMBER = "628971183961";
 
 const plans = [
 	{
